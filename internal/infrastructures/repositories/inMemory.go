342977cb6035743c3f21f4e509d1ba91d101b220
rama <tgcfitrah26@gmail.com>
2023-01-22 00:03:57 +0700

    add repository adapter & implement in memory repository

---
+++ internal/infrastructures/repositories/inMemory.go
@@ -0,0 +1,44 @@
+package repositories
+
+import (
+	"errors"
+	"github.com/mfitrahrmd/pastebin/internal/core/domain"
+	"github.com/mfitrahrmd/pastebin/internal/core/ports"
+)
+
+type inMemoryPastebinRepository struct {
+	pastebin map[string]domain.Pastebin
+}
+
+func NewInMemoryPastebinRepository() ports.PastebinRepository {
+	pr := inMemoryPastebinRepository{
+		pastebin: map[string]domain.Pastebin{},
+	}
+
+	return &pr
+}
+
+func (i *inMemoryPastebinRepository) Save(pastebin domain.Pastebin) error {
+	i.pastebin[pastebin.Shortlink] = pastebin
+
+	return nil
+}
+
+func (i *inMemoryPastebinRepository) Get(shortlink string) (domain.Pastebin, error) {
+	p, ok := i.pastebin[shortlink]
+	if !ok {
+		return domain.Pastebin{}, errors.New("pastebin not found")
+	}
+
+	return p, nil
+}
+
+func (i *inMemoryPastebinRepository) List() ([]domain.Pastebin, error) {
+	pastebin := []domain.Pastebin{}
+
+	for _, p := range i.pastebin {
+		pastebin = append(pastebin, p)
+	}
+
+	return pastebin, nil
+}
